24638347979af8b38ce386a88d06f432d38fadd0
Vamsi <mkrishnadev1996@gmail.com>
2026-04-28 12:36:52 +0530

    take user input and write to file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 print("hello how are you?")
 print("In main file")
+data = input("Enter some data: ")
 with open("file1.txt", "r") as f:
     data = f.read()
 data_capitalized = data.upper()
 print(data_capitalized)
+with open("user_data.txt", "w") as f:
+    f.write(data)
 print("End of main file")
 print("Bye! See you later!")
