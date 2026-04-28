a3e13202acc7d869bea83a679b52e386188eb5a9
Vamsi <mkrishnadev1996@gmail.com>
2026-04-28 12:39:43 +0530

    added try except for file reading

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 print("hello how are you?")
 print("In main file")
-data = input("Enter some data: ")
-with open("file1.txt", "r") as f:
-    data = f.read()
-data_capitalized = data.upper()
-print(data_capitalized)
-with open("user_data.txt", "w") as f:
-    f.write(data)
+try:
+    with open("file1.txt", "r") as f:
+        data = f.read()
+    data_capitalized = data.upper()
+    print(data_capitalized)
+except FileNotFoundError:
+    print("file1.txt not found. Please make sure the file exists.")
+
 print("End of main file")
 print("Bye! See you later!")
